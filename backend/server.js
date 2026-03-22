75507558614010b5587c822b0364f9770f83a799
Nikhil516 <nikhilverma897939@gmail.com>
2026-03-22 13:05:19 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,6 @@
-//thats the first
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
