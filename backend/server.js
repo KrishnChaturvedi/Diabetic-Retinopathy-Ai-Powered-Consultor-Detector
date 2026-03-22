18517bea7574480600311d42370af6fac1f1e3f8
Nikhil516 <nikhilverma897939@gmail.com>
2026-03-22 13:49:56 +0530

    db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,25 @@
 import express from 'express';
-const app = express();
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+import dotenv from 'dotenv';
+import connectDB from './utils/db';
+import cookieparser from 'cookie-parser';
+import cors from 'cors';
+
+//mongodb connection
+dotenv.config();     
+connectDB();
+
+//middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cookieparser());
+const corsOptions = {
+    origin: 'http://localhost:5173',       //frontend server
+    credentials: true,
+    optionSuccessStatus: 200
+}
+
+
+//backend server
+app.listen(process.env.PORT, () => {   
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
